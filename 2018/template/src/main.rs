99e6058c3d044239d748fd98cbb05e5f09c6b39b
MarWit <contact@marwit.rs>
2018-12-01 16:53:49 +0100

    Day 1

---
+++ 2018/template/src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fmt::Display;
+use std::fs;
+use std::io::{BufRead, BufReader};
+
+fn part1<I: Iterator<Item=String>>( input: I ) -> impl Display {
+    42
+}
+
+fn part2<I: Iterator<Item=String>>( input: I ) -> impl Display {
+    42
+}
+
+fn usage() -> &'static str {
+    "usage: cargo run --release -- <part1|part2>"
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let part = env::args().nth( 1 ).unwrap_or_else( || "part1".into() );
+    let first = match part.as_ref() {
+        "part1"     => true,
+        "part2"     => false,
+        _           => return Err( usage().into() )
+    };
+
+    let input_file = env::args().nth( 2 ).unwrap_or_else( || "res/input".into() );
+
+    let reader = BufReader::new( fs::File::open( input_file )? );
+    let mut lines = reader.lines().map( |l| l.expect( "invalid input" ) );
+
+    if first { println!( "{}", part1( &mut lines ) ) }
+    else     { println!( "{}", part2( &mut lines ) ) };
+
+
+    Ok( () )
+}
